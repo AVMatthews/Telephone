19a2c01be9adf29780d0cc86700f97a547e5d3ec
Hunter Bashaw <hunter@blivet.sh>
2018-10-19 13:06:58 -0400

    Initial commit

---
+++ telephone.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func client() {
+
+}
+
+func server() {
+
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: " + os.Args[0] + " <originator> <source> <dest>")
+		return
+	}
+	source := os.Args[2]
+	dest := os.Args[3]
+
+	if os.Args[1] == "1" {
+
+	} else {
+
+	}
+}
