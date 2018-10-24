dd71c14f67bf5f869fa92a68f4385b7891f6b5f3
Hunter Bashaw <hunter@blivet.sh>
2018-10-23 20:09:08 -0400

    Handled handshake on client

--- telephone.go
+++ telephone.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 )
 
+const VERSION string = "1.7"
+
 func client(input chan string, ipaddr string) {
 	conn, err := net.Dial("tcp", ipaddr)
 	if err != nil {
 		panic(err)
+	}
+	defer conn.Close()
+	nin := bufio.NewScanner(bufio.NewReader(conn))
+	nin.Split(bufio.ScanWords)
+	nin.Scan()
+	if nin.Text() != "HELLO" {
+		fmt.Println("Telephone handshake failed")
+		fmt.Fprintf(conn, "QUIT\n")
+	}
+	nin.Scan()
+
+	if nin.Text() != VERSION {
+		fmt.Println("Client version incompatable: " + VERSION + " server version: " + nin.Text())
+		fmt.Fprintf(conn, "QUIT\n")
 	}
 
 }
@@ -23,7 +40,7 @@ func main() {
 		fmt.Println("Usage: " + os.Args[0] + " <originator> <source> <dest>")
 		return
 	}
-	source := os.Args[2]
+	// source := os.Args[2]
 	dest := os.Args[3]
 
 	comm := make(chan string)
